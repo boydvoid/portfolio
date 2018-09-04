87c2a1edc8a808fdcd3ea01c930b9b2e4e4d0034
Bobby Boyd <bboyd2008@gmail.com>
2018-09-03 23:54:13 -0700

    fixed onclick to work with ids

--- assets/main.js
+++ assets/main.js
@@ -1,16 +1,18 @@
 // the onclick for the other "pages"
-var about = document.getElementById('about-container');
-var projects = document.getElementById('projects-container');
-
+var about = document.getElementById('about-bar');
+var projects = document.getElementById('projects-bar');
+var aboutContainer = document.getElementById('about-container');
+var projectsContainer = document.getElementById('projects-container');
 about.onclick = function () {
-    togglePage(about);
+    togglePage(aboutContainer);
 }
 projects.onclick = function () {
-    togglePage(projects);
+    togglePage(projectsContainer);
 }
 // need to add "open" classes for each page
 // set the css of the about and projects containers onclick
 function togglePage(id) {
+    console.log('toggle clicked');
     if (!id.classList.contains('toggle')) {
         id.classList.add('toggle');
     } else {
